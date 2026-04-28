7aeae0f52583f6331f2419074e7ee1cbc5365536
emryll <emryll5@proton.me>
2026-04-29 02:35:09 +0300

    Added banner

--- cli.go
+++ cli.go
@@ -2,10 +2,11 @@ package main
 
 import "fmt"
 
-func ParseCommand(tokens []string) error {
+// @return     exit, err
+func ParseCommand(tokens []string) (bool, error) {
 	//TODO:
 
-	return nil
+	return false, nil
 }
 
 // TODO: overview command
@@ -36,7 +37,6 @@ func CliGetGraphs(snap Snapshot) {
 }
 
 //TODO: view pool command
-//TODO: get nodes with more than n connections
 
 func CliGetByConnection(snap Snapshot, min int) {
 	nodes := GetNodesByNumConnections(snap, min)
@@ -60,4 +60,27 @@ func GetNodesByNumConnections(snap Snapshot, min int) []*ProcessSnapshot {
 		}
 	}
 	return nodes
+}
+
+func PrintBanner(num ...int) {
+	var choice int
+	if len(num) == 0 {
+		choice = DEFAULT_BANNER
+	} else {
+		choice = num[0]
+	}
+	switch choice {
+	case 0:
+		fmt.Println("		                                 d8b 					   ")
+		fmt.Println("                                        ?88 					   ")
+		fmt.Println("                                         88b 					   ")
+		fmt.Println(" d888b8b    88bd88b d888b8b  ?88,.d88b,  888888b  d8888b  88bd88b ")
+		fmt.Println("d8P' ?88    88P'  `d8P' ?88  `?88'  ?88  88P `?8bd8b_,dP  88P'  ` ")
+		fmt.Println("88b  ,88b  d88     88b  ,88b   88b  d8P d88   88P88b     d88      ")
+		fmt.Println("`?88P'`88bd88'     `?88P'`88b  888888P'd88'   88b`?888P'd88'      ")
+		fmt.Println("       )88                     88P'                               ")
+		fmt.Println("      ,88P                    d88                                 ")
+		fmt.Println("  `?8888P                     ?8P                                 ")
+	}
+	fmt.Printf("\t(%s)\n\n", VERSION)
 }
