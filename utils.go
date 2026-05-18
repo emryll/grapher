84160af141f580c28d62533db904941899b53e7d
emryll <emryll5@proton.me>
2026-05-18 23:50:51 +0300

    Added print functions for session info

---
+++ utils.go
@@ -0,0 +1,53 @@
+package main
+
+import "fmt"
+
+// sum / n
+func GetAvgAndMedian(pools []Pool) (float64, float64) {
+	var (
+		total  int
+		median float64
+		avg    float64
+	)
+	//TODO: sort pool by length
+	for i, pool := range pools {
+
+		//TODO: check if i is in middle
+	}
+	avg = float64(total / len(pools))
+
+	return avg, median
+}
+
+func (s Session) PrintDescription() {
+	fmt.Printf("\tRun description: ")
+	if s.Description == "" {
+		fmt.Printf("N/A\n")
+	} else {
+		fmt.Printf("%s\n", s.Description)
+	}
+}
+
+func (s Session) PrintSelected() {
+	if s.Selected == nil {
+		fmt.Printf("\t[*] No snapshot selected (%d available)\n", len(s.Snapshots))
+		for _, snap := range s.Snapshots {
+			fmt.Printf("\t\t- Snapshot %d (%d nodes, +%dms)\n", snap.SnapId, snap.GetNodeCount(), snap.Interval)
+		}
+		fmt.Println()
+		return
+	}
+	fmt.Printf("\t[*] Snapshot %d selected\n", s.Selected.SnapId)
+	fmt.Printf("\t\t: %d separate graphs\n", len(s.Selected.Graphs))
+	fmt.Printf("\t\t: %d nodes total\n", s.Selected.GetNodeCount())
+	fmt.Printf("\t\t: %d connections total\n", s.Selected.GetEdgeCount())
+	fmt.Printf("\t\t: +%dms\n\n", s.Selected.Interval)
+}
+
+func (s Snapshot) GetNodeCount() int {
+	var count int
+	for _, graph := range s.Graphs {
+		count += len(graph)
+	}
+	return count
+}
