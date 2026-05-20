1d18d9e4d1a6d55ea700f78a41343b0c95203a68
emryll <emryll5@proton.me>
2026-05-20 23:39:56 +0300

    Define object access lookup structure

--- graphs.go
+++ graphs.go
@@ -1,5 +1,7 @@
 package main
 
+import "sync"
+
 var (
 	ID_COUNTER             = 1
 	g_ObjectAccessRegistry *ObjectAccessRegistry
@@ -13,12 +15,54 @@ func (c *Connection) Strip(flags Bitmask, weight int) Bitmask {
 	return c.Type
 }
 
-// Add weight or new connection type to a connection.
+// Add weight or new connection type to a connection
 func (c *Connection) Expand(flags Bitmask, weight int) {
 	c.Weight += weight
 	c.Type |= flags
 }
 
+// Check if connection passes given traversal rule
 func (c *Connection) Passes(rule Traversal) bool {
 	return c.Type.HasFlags(rule.flags) && c.Weight >= rule.weight
+}
+
+func (snap Snapshot) CreatePools() []Pool {
+	var pools []Pool
+	for _, g := range snap.Graphs {
+		pools = append(pools, g.CreatePools()...)
+	}
+	return pools
+}
+
+func (g *GraphSnapshot) CreatePools() []Pool {
+	//TODO: traverse graph
+}
+
+//*====================[ Object Access Lookup ]===================
+
+// Lookup table for object interactions
+// 500 000 entries would be around 32MB
+type ObjectAccessRegistry struct {
+	mu sync.RWMutex // used internally in methods
+	// process -> object type -> name -> entry
+	ProcessLookup map[uint32]map[ProcessAccessKey][]*AccessEntry // array is for anon objects
+	// object type -> name -> process -> entry
+	ObjectLookup map[uint32]map[ObjectAccessKey][]*AccessEntry
+}
+
+// With the triple nested map, amount of maps grows very quickly.
+// To fix this issue, the structure is partially flattened.
+// Instead of a triple map its a double map with a struct key,
+// which has a very big effect on the amount of maps created.
+
+// This key struct is made to flatten ProcessLookup
+type ProcessAccessKey struct {
+	ObjType uint32
+	Name    string
+}
+
+// This key struct is made to flatten ObjectLookup
+type ObjectAccessKey struct {
+	Pid  uint32
+	Name string
 }
